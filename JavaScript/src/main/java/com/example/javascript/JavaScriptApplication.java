7fa6d34f88214f7ce6d282044294ccc021759300
sebas1310 <a20203368@pucp.edu.pe>
2023-06-19 23:40:04 -0500

    clase 11

---
+++ JavaScript/src/main/java/com/example/javascript/JavaScriptApplication.java
@@ -0,0 +1,13 @@
+package com.example.javascript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaScriptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaScriptApplication.class, args);
+    }
+
+}
---
+++ JavaScript/src/test/java/com/example/javascript/JavaScriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javascript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaScriptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
